c3a4145aac7f66db91fb318371f94606a6e9d83b
alambrow <alexander.lambrow@gmail.com>
2021-09-17 16:00:10 -0500

    responsive calendar

--- src/components/LandingSite.js
+++ src/components/LandingSite.js
@@ -1,29 +1,71 @@
 import { useContext, useEffect, useState } from "react";
 import { ProfileContext } from "./providers/ProfileProvider";
+import { EssayContext } from "./providers/EssayProvider";
 import Calendar from 'react-calendar';
 import 'react-calendar/dist/Calendar.css';
 
 export const LandingSite = () => {
-    const { profile, getProfile } = useContext(ProfileContext)
-    const [today, setAltDate] = useState(new Date());
+    const { profile, getProfile } = useContext(ProfileContext);
+    const { getUpcomingEssays, getUpcomingEssaysInDateRange } = useContext(EssayContext);
+    const [upcomingEssays, setUpcomingEssays] = useState([]);
+    const [day, setDay] = useState(new Date());
+    const [altDay, setAltDay] = useState();
 
     useEffect(() => {
-        getProfile()
-    }, [])
+        getProfile();
+    }, []);
+
+    useEffect(() => {
+        getUpcomingEssays(3)
+            .then(data => setUpcomingEssays(data));
+    }, []);
+
+    // TODO: getEssaysByDay to account for calendar change
+    useEffect(() => {
+        if (altDay) {
+            getUpcomingEssaysInDateRange(altDay)
+                .then(data => setUpcomingEssays(data))
+        }
+    }, [altDay])
+    
+    const renderEssayItem = essay => {
+        return (
+            <div className="essay_item">
+                {essay.topic}
+                <br/>
+                {essay.student.full_name}
+                <br/>
+                {essay.floating_dd}
+                <br/>
+                {essay.official_dd}
+                <br/> <br/>
+            </div>
+        )
+    }
 
     return (
         <>
-        <main>
-            <div className="profile_outline">
-                <div className="profile_welcome_banner">Welcome, {profile.username}</div>
-            </div>
-            <div>
-            <Calendar
-                onChange={(event) => setAltDate(event)}
-                value={today}
-            />
-            </div>
-        </main>
+            <main>
+                <div className="profile_outline">
+                    <div className="profile_welcome_banner">Welcome, {profile.username}</div>
+                </div>
+                <div>
+                    <Calendar
+                        onChange={(event) => {
+                            const newDay = event.toISOString().split('T')[0]
+                            console.log(newDay)
+                            setAltDay(newDay)
+                            setDay(event)
+                        }}
+                        value={day}
+                    />
+                </div>
+                <div>
+                    {
+                        upcomingEssays.map(essay => renderEssayItem(essay))
+                    }
+                </div>
+            </main>
         </>
     )
 }
