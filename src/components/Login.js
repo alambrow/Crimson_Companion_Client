af0bf9d9f5d4a23a98e3250b7c900f10f16abfd3
alambrow <alexander.lambrow@gmail.com>
2021-09-18 12:08:24 -0500

    styled login, new student, new essay

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,4 +1,6 @@
 import { GoogleLogin } from 'react-google-login';
+import CrimsonIcon from './images/crimson_icon.jpg';
+import './styles/login.css';
 
 export const Login = (props) => {
 
@@ -28,14 +30,32 @@ export const Login = (props) => {
 
   return (
     <>
-      <GoogleLogin
-        clientId="812910456899-89g2l108boob0jtkn1q3ph5tgs46vbkd.apps.googleusercontent.com"
-        buttonText="Login using Google"
-        onSuccess={loginWithGoogleCredentials}
-        onFailure={responseGoogle_error}
-        cookiePolicy={'single_host_origin'}
-        isSignedIn={true}
-      />
+      <main>
+        <div className="login__full_panel">
+          <div className="login__welcome_banner">
+            Crimson Companion App
+            </div>
+            <div className="login__banner_undertitle">
+              An Organizer for College Application Consultants
+            </div>
+          
+          <div className="login__bottom_flex">
+            <div className="login__logo">
+            <img src={CrimsonIcon} justify-content="center" width="100" height="100" />
+            </div>
+            <div className="login__button">
+            <GoogleLogin
+              clientId="812910456899-89g2l108boob0jtkn1q3ph5tgs46vbkd.apps.googleusercontent.com"
+              buttonText="Login using Google"
+              onSuccess={loginWithGoogleCredentials}
+              onFailure={responseGoogle_error}
+              cookiePolicy={'single_host_origin'}
+              isSignedIn={true}
+            />
+            </div>
+          </div>
+        </div>
+      </main>
     </>
   )
 }
